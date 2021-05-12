a3d41bc2af99b1ed09848bdb62ee51d82d521e72
Femboy Hooters® manager <75750909+Femboyhooters-png@users.noreply.github.com>
2021-05-12 19:04:55 +0200

    added a python version uwu

---
+++ uwuifier/uwufier.py
@@ -0,0 +1,9 @@
+import random
+
+uwutext = input("pwease input text uwu: ")
+#replacing letters uwu 
+#TODO add a percentage for stuttering
+uwutext = uwutext.replace("l", "w").replace("r", "w").replace("v", "f").replace("i", "i-i").replace("d", "d-d").replace("n", "n-n") + " >~<"
+
+
+print(uwutext)
