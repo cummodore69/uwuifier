fc92d409ac1c2e1169514d5e6e0b58786d4d3002
Di <urfavdigitalperson@gmail.com>
2021-05-12 19:35:32 +0200

    better formatting and lowercasing

--- uwuifier/uwufier.py
+++ uwuifier/uwufier.py
@@ -1,9 +1,18 @@
 import random
 
 uwutext = input("pwease input text uwu: ")
-#replacing letters uwu 
-#TODO add a percentage for stuttering
-uwutext = uwutext.replace("l", "w").replace("r", "w").replace("v", "f").replace("i", "i-i").replace("d", "d-d").replace("n", "n-n") + " >~<"
+# replacing letters uwu
+# TODO add a percentage for stuttering
+uwutext = (
+    uwutext.lower()
+    .replace("l", "w")
+    .replace("r", "w")
+    .replace("v", "f")
+    .replace("i", "i-i")
+    .replace("d", "d-d")
+    .replace("n", "n-n")
+    + " >~<"
+)
 
 
 print(uwutext)
